9552cbee9f6b44a9b118259790c09519bb4bcdd8
Hariprasath <hariprasath3456@gmail.com>
2023-02-21 14:30:21 +0530

    Initial Commit

---
+++ FocusBindingPWrapperPOC/FocusBindingPWrapperPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBindingPWrapperPOCApp.swift
+//  FocusBindingPWrapperPOC
+//
+//  Created by Guru Mahan on 21/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusBindingPWrapperPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
